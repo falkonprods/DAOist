1bf9cf52d85b558b260d107602aec68b24382af9
Boris <boris@websoftwar.es>
2018-05-22 15:36:12 -0500

    new ost client update

---
+++ packages/facade-counters/index.js
@@ -0,0 +1,70 @@
+const production = process.env.NODE_ENV === 'production'
+if (!production) {
+  require('dotenv').config()
+}
+
+const mongo = require('mongo')
+const DB_COLLECTION_USERS = 'users'
+const fetch = require('node-fetch')
+const OstClient = require('ost-client')
+
+let ostClient = new OstClient(fetch)
+
+function FacadeCounters(ost, mongo) {
+  this.ost = ost
+  this.mongo = mongo
+}
+
+FacadeCounters.prototype.getWineries = async function () {
+  try {
+    let connection = await this.mongo()
+    let db = await connection.db()
+    let collection = db.collection(DB_COLLECTION_USERS)
+    let user = await collection.find({}).toArray()
+    return user.length
+  } catch (error) {
+    throw error
+  }
+}
+
+FacadeCounters.prototype.getVincoins = async function () {
+  try {
+    let ostTokenResponse = await this.ost.token()
+    let ostToken = await ostTokenResponse.json()
+
+    if (ostToken.success === false) {
+      throw new Error(ostToken.err.msg)
+    }
+
+    return ostToken.total_supply
+  } catch (error) {
+    throw new Error(error.message)
+  }
+}
+
+let facadeCounters = new FacadeCounters(ostClient, mongo)
+
+module.exports.facadeCounters = async (event) => {
+  let response = {
+    statusCode: 400,
+    headers: {
+      'Content-type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: null,
+    isBase64Encoded: false
+  }
+
+  try {
+    response.statusCode = 200
+    response.body = JSON.stringify({
+      wineries: await facadeCounters.getWineries(),
+      vincoins: await facadeCounters.getVincoins(),
+      ideas: 0
+    })
+  } catch (error) {
+    response.body = JSON.stringify({error: error.message})
+  }
+
+  return response
+}
--- packages/ost-client/index.js
+++ packages/ost-client/index.js
@@ -1,4 +1,4 @@
-const url = require('url')
+const URL = require('url')
 const crypto = require('crypto')
 const queryString = require('query-string')
 
@@ -38,8 +38,23 @@ OstClient.prototype.usersCreate = function (name) {
   const endpoint = '/users/create'
   let stringToSign = generateQueryString(endpoint, { api_key: apiKey, request_timestamp: createTimeString(), name: name })
   let uri = stringToSign + '&signature=' + generateApiSignature(stringToSign, apiSecret)
-  let params = url.parse(uri).query
+  let params = URL.parse(uri).query
   return this.client(baseUri + endpoint, { method: 'POST', body: new url.URLSearchParams(params) })
+}
+
+/**
+ * @see https://dev.ost.com/docs/api_token.html
+ * @returns {Promise<*>}
+ */
+OstClient.prototype.token = function () {
+  const endpoint = '/users/create'
+  let stringToSign = generateQueryString(endpoint, { api_key: apiKey, request_timestamp: createTimeString() })
+  let uri = stringToSign + '&signature=' + generateApiSignature(stringToSign, apiSecret)
+  let params = URL.parse(uri).query
+  let url = new URL(baseUri + endpoint)
+  url.search = new url.URLSearchParams(params)
+
+  return this.client(url, { method: 'GET' })
 }
 
 module.exports = OstClient
